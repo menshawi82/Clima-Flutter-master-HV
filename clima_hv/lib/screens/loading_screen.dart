1c30a48eeb48ab31041243be463a8e7beae2877f
Hares Yaseen <menshawi82@gmail.com>
2021-06-13 02:12:42 +0300

    4

--- clima_hv/lib/screens/loading_screen.dart
+++ clima_hv/lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
+import 'package:clima_hv/services/location.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,25 +7,34 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low,
-        forceAndroidLocationManager: true);
-    print(position.toString());
+    Location location = Location();
+   await location.getCurrentLocation();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            //Get the current location
-            getLocation();
-          },
-          child: Text('Get Location'),
+    try {
+      String myMargin = '15';
+      return Scaffold(
+        body: Container(
+          margin: EdgeInsets.all(double.parse(myMargin)),
+          color: Colors.green,
         ),
-      ),
-    );
+      );
+    } catch (e) {
+      return Scaffold(
+        body: Container(
+          color: Colors.red,
+          child: Center(child: Text(e.toString())),
+        ),
+      );
+    }
   }
 }
